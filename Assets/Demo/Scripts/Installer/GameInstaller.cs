51aa30cd887d8a42efeab8dad370844cb7b8ba13
gabriele <gabrielerossini90@gmail.com>
2020-08-29 21:24:28 +0200

    First commit. Added Zenject lib in order to maange Ioc.

---
+++ Assets/Demo/Scripts/Installer/GameInstaller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class GameInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        
+    }
+}
---
+++ Assets/Demo/Scripts/Object/MainCube.cs
@@ -0,0 +1,21 @@
+﻿﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCube : MonoBehaviour
+{
+    public float Speed = 5f;
+    public Rigidbody2D Rb;
+    private Vector2 movement;
+
+    private void Update()
+    {
+        this.movement.x = Input.GetAxisRaw("Horizontal");
+        this.movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    public void FixedUpdate()
+    {
+        this.Rb.MovePosition(this.Rb.position + this.movement * (this.Speed * Time.fixedDeltaTime));
+    }
+}
